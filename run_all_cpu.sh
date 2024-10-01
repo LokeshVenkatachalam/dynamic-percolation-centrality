967340bf8a547d03de1215f93bfb715bbe847b5e
lokeshVenkatachalam <lokesh.v@rce.iiit.ac.in>
2024-10-01 22:10:06 +0530

    Updated cpu scripts

---
+++ run_all_cpu.sh
@@ -0,0 +1,8 @@
+sh ./scripts/cpu\ scripts/PGPgiantcompo.sh
+sh ./scripts/cpu\ scripts/web-webbase-2001.sh
+sh ./scripts/cpu\ scripts/web-indochina-2004.sh
+sh ./scripts/cpu\ scripts/p2p-Gnutella25.sh
+sh ./scripts/cpu\ scripts/p2p-Gnutella30.sh
+sh ./scripts/cpu\ scripts/slashdot.sh
+sh ./scripts/cpu\ scripts/email-enron.sh
+sh ./scripts/cpu\ scripts/brightkite.sh
